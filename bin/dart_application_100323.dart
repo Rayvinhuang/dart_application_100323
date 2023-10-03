a8f68622d7fb3eb644a6f8c6a4b7bbe300fe59a8
Rayvinhuang <rayvinsuhartoyo_2226250007@mhs.mdp.ac.id>
2023-10-03 12:23:54 +0700

    Initial Project

---
+++ bin/dart_application_100323.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_100323/dart_application_100323.dart' as dart_application_100323;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_100323.calculate()}!');
+}
